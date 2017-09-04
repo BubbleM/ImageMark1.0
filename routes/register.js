5da1e3efdb84b8b910bf04df26564c5395be3d99
BubbleM <1870408473@qq.com>
2017-09-04 14:56:06 +0800

    add register controller

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/util/DBConfig');
 
 router.get('/', function(req, res, next) {
   res.render('register', '');
+});
+router.post('/', function (req, res) {
+  let username = req.body.username;
+  let pwd1 = req.body.pwd1;
+  let pwd2 = req.body.pwd2;
+  if(pwd1 === pwd2) {
+    let sql = `INSERT INTO user(username,password,status,descn) values('${username}','${pwd1}','1','用户')`;
+    db.query(sql, function (err, rows) {
+      res.redirect('/login');
+      console.log('插入用户成功');
+    });
+  } else{
+    console.log('两次密码输入不一致，请重新输入');
+  }
 });
 
 module.exports = router;
