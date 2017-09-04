f91fe416c687a1bc3b75f7e333fab8757293a96c
BubbleM <1870408473@qq.com>
2017-09-04 15:36:01 +0800

    clear dao

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../db/dao/User');
 
 router.get('/', function(req, res, next) {
-  let user = new User();
-  user.find();
   res.send('HAHA');
 });
 
